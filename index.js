9eb7600c5126eac0e3cd978bf5de83a4176eed18
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-10 17:49:00 +0600

    dor end user pass updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
